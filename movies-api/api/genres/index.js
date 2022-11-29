1fd7e334b4e510cac803583382737b3eeb58f023
Ben Capper <Bencapper19@gmail.com>
2022-11-29 17:25:15 +0000

    Mongo Lab Exercise Done

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
-import { genres } from './genresData';
+import Genre from './genreModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); 
 
-router.get('/', (req, res) => {  
-    res.json(genres);
+// Get all genres
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
+
+// Update a genre
+router.put('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Genre.updateOne({
+        _id: req.params.id,
+    }, req.body);
+    if (result.matchedCount) {
+        res.status(200).json({ code:200, msg: 'Genre Updated Sucessfully' });
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to Update Genre' });
+    }
+  });
 
 export default router;
